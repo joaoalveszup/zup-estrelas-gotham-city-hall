e3dee5af4789fa7c281688c50cc3337ab88689ec
gabrielgarciazup <gabriel.garcia@zup.com.br>
2020-11-11 14:50:48 -0300

    Spring initializr

---
+++ src/main/java/br/com/zup/estrelas/gothamcityhall/ZupEstrelasGothamCityHallApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.gothamcityhall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupEstrelasGothamCityHallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupEstrelasGothamCityHallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/estrelas/gothamcityhall/ZupEstrelasGothamCityHallApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.gothamcityhall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupEstrelasGothamCityHallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
